c02a865abf205668ff6d0874fa108e57f521218c
Jaachi Okafor <okaforjaachi@gmail.com>
2023-09-11 23:04:37 +0100

    minor refactor

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -8,7 +8,7 @@ const HeroSection = () => {
       <section className=" h-screen bg-section1bg bg-cover z-40 ">
         <Header />
         <div className="max-w-4xl mx-auto ">
-          <div className=" mt-28  mx-8 flex items-center justify-center ">
+          <div className=" mt-28  mx-8 flex items-center justify-center  lg:ml[91.4rem] lg:mt-[12.25rem] ">
             <img src={HeroImage} alt="" srcset="" className=" " />
           </div>
         </div>
